55c58b6d53768779efde61265ec6c82d7de94676
Yannick Couzinie <yannick.couzinie@uniroma3.it>
2022-04-07 12:39:19 +0200

    Implement basic user thread switching

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  // save return point
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved registers
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
+  struct context context;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *, struct context *);
               
 void 
 thread_init(void)
@@ -28,6 +48,12 @@ thread_init(void)
   // save thread 0's state.  thread_schedule() won't run the main thread ever
   // again, because its state is set to RUNNING, and thread_schedule() selects
   // a RUNNABLE thread.
+  struct thread *t;
+
+  for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
+    t->state = FREE;
+  }
+
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
 }
@@ -41,8 +67,9 @@ thread_schedule(void)
   next_thread = 0;
   t = current_thread + 1;
   for(int i = 0; i < MAX_THREAD; i++){
-    if(t >= all_thread + MAX_THREAD)
+    if(t >= all_thread + MAX_THREAD){
       t = all_thread;
+    }
     if(t->state == RUNNABLE) {
       next_thread = t;
       break;
@@ -59,10 +86,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(&t->context, &next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +100,9 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  /* assign to the end since stacks grow backwards */
+  t->context.sp = (uint64) &t->stack[STACK_SIZE];
+  t->context.ra = (uint64) func;
 }
 
 void 
